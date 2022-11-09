22e7c075498d211692ec1f4a37f7bc0dd9915555
Purna Shrestha <crush9918@gmail.com>
2022-11-10 00:14:13 +0545

    added sectional title shadow and added skills new things and upgraded features and bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,6 +156,7 @@
   /**
    * Skills animation
    */
+  /*
   let skilsContent = select(".skills-content");
   if (skilsContent) {
     new Waypoint({
@@ -169,6 +170,43 @@
       },
     });
   }
+  */
+
+  /**
+   * Clients Slider
+   */
+  new Swiper(".clients-slider", {
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
 
   /**
    * Porfolio isotope and filter
