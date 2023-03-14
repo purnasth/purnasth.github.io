794402f2911ef13a99ee0a3d6f3e949c4e142065
Purna Shrestha <crush9918@gmail.com>
2023-03-14 14:42:13 +0545

    updated the services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -310,3 +310,28 @@
    */
   new PureCounter();
 })();
+
+/*--------------------------------------------------------------
+# Services UPDATED
+--------------------------------------------------------------*/
+$(document).ready(function () {
+  $(".circle-container")
+    .find("a")
+    .click(function (event) {
+      event.preventDefault();
+    });
+  $(".circle-container")
+    .find("li")
+    .hover(function () {
+      $(".circle-container").find("li").removeClass("active");
+      $(this).addClass("active");
+      $(".services-container")
+        .find("li.active")
+        .removeClass("active animated fadeIn");
+
+      $(".services-container")
+        .find("li")
+        .eq($(this).data("id"))
+        .addClass("active animated fadeIn");
+    });
+});
