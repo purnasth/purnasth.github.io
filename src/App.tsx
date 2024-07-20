54ebf863b63f67f8c27b1531fec888ba8b61cb83
Purna Shrestha <crush9918@gmail.com>
2024-07-20 21:00:42 +0545

    feat: Add hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Hero from "./components/Hero";
 import "./index.css";
+import Hero from "./components/Hero";
+import gradientBg from "./assets/gradients/gradient1.avif";
 
 const App = () => {
   return (
-    <>
+    <main
+      className={`pointer-events-none absolute inset-0 bg-[url('')] size-full bg-cover bg-center -z-10 mix-blend-darken`}
+      style={{ backgroundImage: `url(${gradientBg})` }}
+    >
       <Hero />
-    </>
+    </main>
   );
 };
 
