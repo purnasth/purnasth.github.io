c431a7f5cb53d8f0f471ebc547e4eac95c766c25
Purna Shrestha <crush9918@gmail.com>
2024-07-29 22:52:01 +0545

    feat: Update about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ProfileNav from "../components/ProfileNav";
+import purna from "../assets/img/purna/FC_20221024_0004.jpg";
 
 const About: React.FC = () => {
   return (
-    <main className="size-full h-screen bg-black text-white">
-      <div className="w-[calc(100%-14rem-5rem)] h-full ml-auto flex items-center justify-center">
+    <main className="size-full h-full bg-black text-white">
+      <div className="w-[calc(100%-14rem-5rem)] h-full ml-auto p-8">
         <ProfileNav />
-        <h1 className="text-5xl">This is an About Page.</h1>
+        <h2 className="text-6xl font-luxury leading-snug">
+          Hey, <br /> I'm Purna Shrestha.
+        </h2>
+        <p className="my-12 ml-32 text-2xl">
+          ---------------- a software engineer based in Kathmandu, Nepal. Lorem
+          ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nam
+          eligendi amet quam eos totam, blanditiis dolores commodi hic quo?
+        </p>
+
+        <img src={purna} alt="Purna" className="w-full h-[42rem] object-cover rounded-3xl"/>
       </div>
     </main>
   );
