4db670f595d12c6738c8f43549c218888f6caef5
Purna Shrestha <crush9918@gmail.com>
2024-08-02 09:37:02 +0545

    feat: Update portolfio page

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import hotelHimalaya from "../assets/img/portfolio/hotel-himalaya.png";
 import { Link } from "react-router-dom";
+import { MdOutlineArrowCircleRight } from "react-icons/md";
 
 const Portfolio: React.FC = () => {
   return (
@@ -15,21 +16,122 @@ const Portfolio: React.FC = () => {
           </div>
         </div>
 
-        <div className="portfolio px-8 my-24">
-          <div className="grid grid-cols-4">
+        <div className="portfolio px-8 my-24 space-y-12">
+          <div className="flex items-center justify-between">
+            <h4 className="text-gold-gradient text-5xl font-luxury">
+              Hotel Himalaya
+            </h4>
+            <Link
+              to="#"
+              className="py-2 px-5 rounded-full flex items-center gap-2 bg-white/20 text-white outline outline-2 outline-white/80 pointer-events-auto hover-outline"
+            >
+              Detail Portfolio
+              <MdOutlineArrowCircleRight className="text-2xl" />
+            </Link>
+          </div>
+          <div className="grid grid-cols-4 gap-8">
             <div className="col-span-3 relative rounded-3xl overflow-hidden group">
               <div>
-                <img src={hotelHimalaya} alt="" className="r" />
+                <img
+                  src={hotelHimalaya}
+                  alt=""
+                  className="size-full object-cover group-hover:scale-125 transition-700"
+                />
                 <div className="overlay group-hover:bg-black/0" />
               </div>
-              <div className="absolute bottom-0 size-full p-8 w-full h-60" >
-                <div className="w-full h-16 flex items-center justify-between gap-5 bg-red-600 px-8 rounded-full">
+              <div className="w-full h-20 absolute bottom-6 px-6">
+                <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
                   <h4>Hotel Himalaya</h4>
                   <Link to="#">Learn more</Link>
                 </div>
               </div>
             </div>
-            <div className="col-span-1"></div>
+            <div className="col-span-1 relative rounded-3xl overflow-hidden group">
+              <div>
+                <img
+                  src={hotelHimalaya}
+                  alt=""
+                  className="size-full object-cover group-hover:scale-125 transition-700"
+                />
+                <div className="overlay group-hover:bg-black/0" />
+              </div>
+              <div className="w-full h-20 absolute bottom-6 px-6">
+                <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
+                  <h4>Hotel Himalaya</h4>
+                  <Link to="#">Learn more</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-8">
+            <div className="col-span-2">
+              <div className="relative rounded-3xl overflow-hidden group">
+                <div>
+                  <img
+                    src={hotelHimalaya}
+                    alt=""
+                    className="size-full object-cover group-hover:scale-125 transition-700"
+                  />
+                  <div className="overlay group-hover:bg-black/0" />
+                </div>
+                <div className="w-full h-20 absolute bottom-6 px-6">
+                  <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
+                    <h4>Hotel Himalaya</h4>
+                    <Link to="#">Learn more</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-8">
+                <div className="col-span-1 relative rounded-3xl overflow-hidden group">
+                  <div>
+                    <img
+                      src={hotelHimalaya}
+                      alt=""
+                      className="size-full h-[38rem] group-hover:scale-125 transition-700"
+                    />
+                    <div className="overlay group-hover:bg-black/0" />
+                  </div>
+                  <div className="w-full h-20 absolute bottom-6 px-6">
+                    <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
+                      <h4>Hotel Himalaya</h4>
+                      <Link to="#">Learn more</Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 relative rounded-3xl overflow-hidden group">
+                  <div>
+                    <img
+                      src={hotelHimalaya}
+                      alt=""
+                      className="size-full h-[38rem] group-hover:scale-125 transition-700"
+                    />
+                    <div className="overlay group-hover:bg-black/0" />
+                  </div>
+                  <div className="w-full h-20 absolute bottom-6 px-6">
+                    <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
+                      <h4>Hotel Himalaya</h4>
+                      <Link to="#">Learn more</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-2 relative rounded-3xl overflow-hidden group">
+              <div>
+                <img
+                  src={hotelHimalaya}
+                  alt=""
+                  className="w-auto h-[72rem] group-hover:scale-125 transition-700"
+                />
+                <div className="overlay group-hover:bg-black/0" />
+              </div>
+              <div className="w-full h-20 absolute bottom-6 px-6">
+                <div className="flex items-center justify-between gap-5 border border-white/50 bg-black/30 backdrop-blur-sm p-6 rounded-full">
+                  <h4>Hotel Himalaya</h4>
+                  <Link to="#">Learn more</Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
