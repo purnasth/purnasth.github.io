4e21b178a7f33c24f67b3bdb1d1733e68c6e24f7
Purna Shrestha <crush9918@gmail.com>
2024-08-04 22:34:58 +0545

    feat: Add mithila yatri niwas

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,5 +1,6 @@
 import hotelHimalaya from "../assets/img/portfolio/hotel-himalaya.png";
 import hotelDayatra from "../assets/img/portfolio/hotel-dayatra.png";
+import mithilaYatriNiwas from "../assets/img/portfolio/mithila.png";
 
 export const portfolioData = [
   {
@@ -57,7 +58,7 @@ export const portfolioData = [
         logoSrc: "https://www.purnashrestha.com.np/dayatra/assets/img/logo.svg",
         portfolioUrl: "hotel-dayatra",
         description:
-        "Hotel Dayatra Courtyard is a luxury hotel of amazing service and perfect for unwinding. The hotel is located in the heart of the pokhara, just 5 minutes away from the airport.",
+          "Hotel Dayatra Courtyard is a luxury hotel of amazing service and perfect for unwinding. The hotel is located in the heart of the pokhara, just 5 minutes away from the airport.",
         landingImage: [
           {
             url: hotelDayatra,
@@ -76,6 +77,49 @@ export const portfolioData = [
           },
           {
             url: hotelDayatra,
+            alt: "Swimming Pool 4",
+            colSpan: 2,
+          },
+        ],
+        technologies: [
+          "Figma",
+          "React.js",
+          "TailwindCSS",
+          "API",
+          "React Router",
+          "TypeScript",
+          "HSTS",
+          "Google Fonts API",
+          "Cloudflare",
+          "Lenis",
+        ],
+        technologiesColor: "#00daff42",
+      },
+      {
+        id: "mithila-yatri-niwas",
+        title: "Mithila Yatri Niwas",
+        logoSrc: "https://www.mithilayatriniwas.com/assets/logo-BoMl5AAK.svg",
+        portfolioUrl: "mithila-yatri-niwas",
+        description:
+          "Mithila Yatri Niwas - A luxurious retreat in Janakpur, just a 10-minute walk from the sacred Janaki Temple. Discover the allure of Janakpur at Mithila Yatri Niwas, where luxury meets legacy just steps away from the divine Janaki Temple and the peaceful Dashrath Lake. Nestled in the city’s vibrant heart, our hotel is a sanctuary of modern comforts, offering unparalleled access to sacred sites and cultural treasures.",
+        landingImage: [
+          {
+            url: mithilaYatriNiwas,
+            alt: "Swimming Pool 1",
+            colSpan: 2,
+          },
+          {
+            url: mithilaYatriNiwas,
+            alt: "Swimming Pool 2",
+            colSpan: 3,
+          },
+          {
+            url: mithilaYatriNiwas,
+            alt: "Swimming Pool 3",
+            colSpan: 3,
+          },
+          {
+            url: mithilaYatriNiwas,
             alt: "Swimming Pool 4",
             colSpan: 2,
           },
