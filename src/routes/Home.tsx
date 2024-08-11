00505f047f3168937ad438ad15d7e9586debba79
Purna Shrestha <crush9918@gmail.com>
2024-08-11 19:17:51 +0545

    feat: Add menu on the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,15 @@
 import Hero from "../components/Hero";
 import ProfileNav from "../components/ProfileNav";
 import SideNav from "../components/SideNav";
+import NavbarButton from "../components/ui/NavbarButton";
 import withFramerTransition from "../hoc/withFramerTransition";
 
 const Home = () => {
   return (
     <>
+      <div className="fixed right-5 top-5 z-50 flex items-center justify-center">
+        <NavbarButton />
+      </div>
       <ProfileNav />
       <Hero />
       <SideNav />
