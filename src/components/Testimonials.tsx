7519bceaa35896bdf76f42d7f666d5b2741fec33
Purna Shrestha <crush9918@gmail.com>
2025-01-28 22:31:41 +0545

    feat: Update the testimonials responsive

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -14,31 +14,31 @@ const Testimonials = () => {
         heading="Clients' Storytellings & Project Impacts"
         styles="px-4 md:px-0 text-light text-3xl md:text-6xl xl:text-7xl font-luxury font-extralight leading-snug text-pretty md:leading-snug"
       />
-      <section className="grid grid-cols-3 gap-6">
-        <div className="col-span-2 space-y-6">
+      <section className="grid xl:grid-cols-3 gap-6">
+        <div className="xl:col-span-2 space-y-4 xl:space-y-6">
           {portfolioDetails.map(
             (portfolio, index) =>
               index % 3 !== 2 && (
                 <div
                   key={index}
-                  className="transition-300 group flex items-start gap-12 rounded-2xl border border-light/20 bg-light/10 p-12 backdrop-blur hover:bg-light/20"
+                  className="transition-300 group flex flex-col xl:flow-row items-start gap-6 xl:gap-12 rounded-2xl border border-light/20 bg-light/10 p-6 xl:p-12 backdrop-blur hover:bg-light/20"
                 >
-                  <span className="transition-300 flex aspect-square size-14 items-center justify-center rounded-full border bg-light text-center text-dark group-hover:border-light/50 group-hover:bg-light/30 group-hover:text-light">
-                    <FaQuoteLeft className="text-3xl" />
+                  <span className="transition-300 flex aspect-square size-10 xl:size-14 items-center justify-center rounded-full border bg-light text-center text-dark group-hover:border-light/50 group-hover:bg-light/30 group-hover:text-light">
+                    <FaQuoteLeft className="text-xl xl:text-3xl" />
                   </span>
                   <div>
-                    <blockquote className="text-pretty font-body text-xl font-extralight">
+                    <blockquote className="text-pretty font-body text-base md:text-xl font-extralight">
                       {portfolio.caseStudy.feedback.clientReview}
                     </blockquote>
-                    <div className="mt-8 flex items-center justify-between">
-                      <div className="flex items-center gap-4">
+                    <div className="mt-4 xl:mt-8 flex items-center justify-between flex-col xl:flex-row gap-y-8">
+                      <div className="flex w-full xl:items-center gap-4 xl:gap-8">
                         <img
                           src={portfolio.logoSrc}
                           alt={portfolio.caseStudy.feedback.author}
-                          className="size-24 object-contain"
+                          className="size-20 xl:size-24 object-contain"
                         />
                         <div className="flex flex-col gap-4">
-                          <h3 className="font-body text-2xl font-normal">
+                          <h3 className="font-body text-xl xl:text-2xl font-normal">
                             {portfolio.caseStudy.feedback.author}
                           </h3>
                           <ul className="flex items-center gap-2">
@@ -57,7 +57,7 @@ const Testimonials = () => {
                       </div>
                       <Link
                         to={`/portfolio/${portfolio.portfolioUrl}`}
-                        className="transition-300 rounded-full border border-light/50 bg-light/20 px-10 py-3 text-light hover:bg-dark hover:text-light group-hover:bg-light group-hover:text-dark"
+                        className="transition-300 w-full xl:w-auto text-center min-w-fit rounded-full border border-light/50 bg-light/20 px-10 py-3 text-light hover:bg-dark hover:text-light group-hover:bg-light group-hover:text-dark"
                       >
                         View Case Study
                       </Link>
@@ -73,24 +73,24 @@ const Testimonials = () => {
               index % 3 === 2 && (
                 <div
                   key={index}
-                  className="transition-300 group flex flex-col items-start gap-12 rounded-2xl border border-light/20 bg-light/10 p-12 backdrop-blur hover:bg-light/20"
+                  className="transition-300 group flex flex-col items-start gap-12 rounded-2xl border border-light/20 bg-light/10 p-6 xl:p-12 backdrop-blur hover:bg-light/20"
                 >
-                  <span className="transition-300 flex aspect-square size-14 items-center justify-center rounded-full border bg-light text-center text-dark group-hover:border-light/50 group-hover:bg-light/30 group-hover:text-light">
-                    <FaQuoteLeft className="text-3xl" />
+                  <span className="transition-300 flex aspect-square size-10 xl:size-14 items-center justify-center rounded-full border bg-light text-center text-dark group-hover:border-light/50 group-hover:bg-light/30 group-hover:text-light">
+                    <FaQuoteLeft className="text-xl xl:text-3xl" />
                   </span>
                   <div>
-                    <blockquote className="text-pretty font-body text-xl font-extralight">
+                  <blockquote className="text-pretty font-body text-base md:text-xl font-extralight">
                       {portfolio.caseStudy.feedback.clientReview}
                     </blockquote>
-                    <div className="mt-8 flex flex-col items-start justify-between gap-8">
-                      <div className="flex items-center gap-4">
+                    <div className="mt-4 xl:mt-8 flex items-center justify-between flex-col gap-y-8">
+                      <div className="flex w-full xl:items-center gap-4">
                         <img
                           src={portfolio.logoSrc}
                           alt={portfolio.caseStudy.feedback.author}
-                          className="size-24 object-contain"
+                          className="size-20 xl:size-24 object-contain"
                         />
                         <div className="flex flex-col gap-4">
-                          <h3 className="font-body text-2xl font-normal">
+                        <h3 className="font-body text-xl xl:text-2xl font-normal">
                             {portfolio.caseStudy.feedback.author}
                           </h3>
                           <ul className="flex items-center gap-2">
@@ -109,8 +109,8 @@ const Testimonials = () => {
                       </div>
                       <Link
                         to={`/portfolio/${portfolio.portfolioUrl}`}
-                        className="transition-300 w-full rounded-full border border-light/50 bg-light/20 px-6 py-3 text-light hover:bg-dark hover:text-light group-hover:bg-light group-hover:text-dark"
-                      >
+                        className="w-full text-center transition-300 rounded-full border border-light/50 bg-light/20 px-10 py-3 text-light hover:bg-dark hover:text-light group-hover:bg-light group-hover:text-dark"
+                        >
                         View Case Study
                       </Link>
                     </div>
